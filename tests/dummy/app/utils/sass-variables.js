696d0d423f793474238af24811ba88d40fea1b33
David Pett <dpett@folklore.digital>
2017-01-03 17:16:02 -0600

    added jshint ignore to pass tests in consuming app

--- tests/dummy/app/utils/sass-variables.js
+++ tests/dummy/app/utils/sass-variables.js
@@ -1,4 +1,6 @@
+/* jshint ignore:start */
 // DON'T UPDATE THIS FILE MANUALLY, IT IS AUTO-GENERATED.
 const sassVariables = JSON.parse(`{"colorRed":"#FF4136","colorBlue":"#fc1234","fontSerif":"'Scope One', serif"}`);
 
 export default sassVariables;
+/* jshint ignore:end */
